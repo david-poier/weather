a4f66f482d3eaa4d09bee5d8063b9f8b709391fe
David-Poier <davidpoier3@gmail.com>
2023-12-13 16:41:34 -0300

    Update scipts.js

--- scipts.js
+++ scipts.js
@@ -11,14 +11,14 @@ function colocarDadosNaTela(dados){
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
-async function buscarCidade(cidade) {
+async function buscarCidade(cidade){
 
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
    
     colocarDadosNaTela(dados)
 }
 
-function cliqueiNoBotao () {
+function cliqueiNoBotao (){
     const cidade = document.querySelector(".input-cidade").value
 
     buscarCidade(cidade)
